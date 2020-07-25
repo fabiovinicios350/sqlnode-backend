34773762486aaf83cc54ca3e43a7c655736f9053
Fabio Vincios Pereira Barbosa <fabiovinicius350@gmail.com>
2020-07-25 16:18:31 -0300

    deploy

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
- web: yarn start 
- 
+ web: yarn start  
--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv/config');
+
 module.exports= { 
-  dialect: 'mssql',
-  host: 'localhost',
-  username: 'sa',
-  password: 'fabioFuba1998',
-  database: 'sqlnode',
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   define: {
     timestamps:true,
     underscored: true,
